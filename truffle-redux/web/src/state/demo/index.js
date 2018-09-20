c61082a5746b33e9ec3c11544e6b74a38f62b0df
Dennison Bertram <dennison@dennisonbertram.com>
2018-09-20 16:59:27 -0400

    Start With IPFS Reducer Tests

--- truffle-redux/web/src/state/demo/index.js
+++
@@ -1,6 +0,0 @@
-import { demoReducer } from './reducers';
-
-export { default as demoTypes } from './types';
-export { default as demoActions } from './actions';
-
-export default demoReducer;
--- truffle-redux/web/src/state/demo/types.js
+++
@@ -1,7 +0,0 @@
-const GET_DATA = 'GET_DATA';
-const SEND_DATA = 'SEND_DATA';
-
-export default {
-  GET_DATA,
-  SEND_DATA
-};
