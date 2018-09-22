5a6e8bbd53a3a78dce72184204bd36b10a0f94b7
Dennison Bertram <dennison@dennisonbertram.com>
2018-09-22 16:00:08 -0400

    Added The contract to the frontend

--- truffle-redux/web/src/index.js
+++ truffle-redux/web/src/index.js
@@ -9,10 +9,12 @@ import { updateWeb3Status, getUserAccounts_THUNK} from './state/web3/actions';
 import {IPFS_ready} from './state/IPFS/actions';
 import registerServiceWorker from './registerServiceWorker';
 //import OrbitDB from 'orbit-db'
-import runOrbit from './OrbitDB'
-
-//ipfs
+import runOrbit from './OrbitDB';
+//must be a simpler ;way
+import OppStoreJson from './contracts/OppStore.json';
 import IPFSNODE from './ipfs'
+
+console.log("The OPP: ", OppStoreJson.abi)
 
 ReactDOM.render(
   <Provider store={store}>
@@ -26,6 +28,11 @@ window.addEventListener('load', async () => {
   // could remove and only use lib/web3utils
   const hasWeb3 = typeof window.web3 !== 'undefined';
   const web3 = hasWeb3 ? new Eth(window.web3.currentProvider) : null;
+
+  const oppStoreContract = web3.contract(OppStoreJson.abi)
+  const oppStore = oppStoreContract.at('0x4e72770760c011647d4873f60a3cf6cdea896cd8');
+
+  console.log("oppStore: ", oppStore);
 
   IPFSNODE.once('ready', async ()=> {
     console.log("IPFS IS READY")
